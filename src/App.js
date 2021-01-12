2390bb6807a20ec4ee3f8b0e3b62ec4feceb6ba2
riymuh <mriyadh103@gmail.com>
2021-01-12 14:24:16 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=84592cf2007007a499b04d12d281c100&page=1";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=84592cf2007007a499b04d12d281c100&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <input type="sarch" className="search" placeholder="Search..." />
       </header>
-    </div>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          // <Movie
+          //   key={movie.id}
+          //   poster_path={movie.poster_path}
+          //   title={movie.title}
+          // />
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   );
 }
 
